3b8701118039a71a6c9cfda30af1b2c8b28927d4
Mahdi Amiri <44555777+MahdiAmiri9737@users.noreply.github.com>
2026-05-26 17:53:36 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,16 @@
 import numpy as np
 import logging
 
-logging.basicConfig(filename= 'mmmmm.log',
-                      format= '%(asctime)s %(message)s',
-                      level= logging.INFO,
-                      filemode= 'w')
-  
-logger_test = logging.getLogger("test") 
 
-logger_test.info(f'asdadsadasdadsasdasd')
+
+with open("example.txt", "w") as file:
+    pass
+
+# logging.basicConfig(filename= 'mmmmm.log',
+#                       format= '%(asctime)s %(message)s',
+#                       level= logging.INFO,
+#                       filemode= 'w')
+  
+# logger_test = logging.getLogger("test") 
+
+# logger_test.info(f'asdadsadasdadsasdasd')
