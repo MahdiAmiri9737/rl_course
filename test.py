a5097e90c0649c6fa44a571d35d3fb54879585f9
Mahdi Amiri <44555777+MahdiAmiri9737@users.noreply.github.com>
2026-05-26 17:22:39 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,11 @@
 import numpy as np
+import logging
 
+logging.basicConfig(filename= 'mmmmm.log,
+                      format= '%(asctime)s %(message)s',
+                      level= logging.INFO,
+                      filemode= 'w')
+  
+logger_test = logging.getLogger("test") 
 
-print('dsfsdfs')
+logger_test.info(f'asdadsadasdadsasdasd')
